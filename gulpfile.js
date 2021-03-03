94d47bb5c97b5414e68c72d0f899a54847becc77
Lucas Pradel <lucaspradel@gmail.com>
2021-03-03 23:49:58 +0100

    Update to gulp 4.0 and deployment via gh pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,193 @@
-const gulp = require('gulp');
-const handlebars = require('gulp-compile-handlebars');
-const watch = require('gulp-watch');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css');
-const gulpif = require('gulp-if');
-const less = require('gulp-less');
-const pump = require('pump');
+const gulp = require("gulp");
+const handlebars = require("gulp-compile-handlebars");
+const watch = require("gulp-watch");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const gulpif = require("gulp-if");
+const less = require("gulp-less");
+const pump = require("pump");
 const rename = require("gulp-rename");
-const autoprefixer = require('gulp-autoprefixer');
-const runSequence = require('run-sequence');
-const sitemap = require('gulp-sitemap');
+const autoprefixer = require("gulp-autoprefixer");
+const sitemap = require("gulp-sitemap");
 
-const templateData = require('./src/data/context.json');
+const templateData = require("./src/data/context.json");
 
-gulp.task('build-html', function() {
+gulp.task("build-html", function () {
   const options = {
     // partials (reusable files - are referenced like {{> header}})
-    batch: ['./src/templates/', './src/templates/icons/']
-  }
+    batch: ["./src/templates/", "./src/templates/icons/"],
+  };
 
-  return gulp.src('src/*.html')
+  return gulp
+    .src("src/*.html")
     .pipe(handlebars(templateData, options))
-    .pipe(gulp.dest('dist'))
-    .pipe(sitemap({
-      siteUrl: process.env.URL || 'http://quodera.com'
-    }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest("dist"))
+    .pipe(
+      sitemap({
+        siteUrl: process.env.URL || "http://quodera.com",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build-js', function(cb) {
+gulp.task("build-js", function (cb) {
   const srcFiles = [
-    'src/js/libs/jquery-3.2.1.js',
-    'src/js/libs/*.js',
-    'src/js/scripts.js'
-  ]
-  pump([
-    gulp.src(srcFiles),
-    concat('bundle.js'),
-    gulpif(process.env.NODE_ENV === 'production', uglify()),
-    gulp.dest('dist')
-  ], cb);
+    "src/js/libs/jquery-3.2.1.js",
+    "src/js/libs/*.js",
+    "src/js/scripts.js",
+  ];
+  pump(
+    [
+      gulp.src(srcFiles),
+      concat("bundle.js"),
+      gulpif(process.env.NODE_ENV === "production", uglify()),
+      gulp.dest("dist"),
+    ],
+    cb
+  );
 });
 
-gulp.task('compile-less', function(cb) {
-  pump([
-    gulp.src('src/css/styles.less'),
-    less(),
-    autoprefixer(),
-    gulp.dest('src/css')
-  ], cb);
+gulp.task("compile-less", function (cb) {
+  pump(
+    [
+      gulp.src("src/css/styles.less"),
+      less(),
+      autoprefixer(),
+      gulp.dest("src/css"),
+    ],
+    cb
+  );
 });
 
-gulp.task('build-css', ['compile-less'], function(cb) {
-  const srcFiles = [
-    'src/css/libs/bootstrap.min.css',
-    'src/css/libs/*.css',
-    'src/css/styles.css',
-  ]
-  pump([
-    gulp.src(srcFiles),
-    concat('bundle.css'),
-    gulpif(process.env.NODE_ENV === 'production', cleanCSS()),
-    gulp.dest('dist')
-  ], cb);
-});
+gulp.task(
+  "build-css",
+  gulp.series("compile-less", function (cb) {
+    const srcFiles = [
+      "src/css/libs/bootstrap.min.css",
+      "src/css/libs/*.css",
+      "src/css/styles.css",
+    ];
+    pump(
+      [
+        gulp.src(srcFiles),
+        concat("bundle.css"),
+        gulpif(process.env.NODE_ENV === "production", cleanCSS()),
+        gulp.dest("dist"),
+      ],
+      cb
+    );
+  })
+);
 
-gulp.task('build-assets', function(cb) {
-  pump([
-    gulp.src('src/assets/**/*'),
-    gulp.dest('dist/assets/')
-  ]);
+gulp.task(
+  "build-assets",
+  gulp.parallel(copyAssets, copyFonts, copyFavicon, copyIcons)
+);
 
-  pump([
-    gulp.src('src/fonts/**/*'),
-    gulp.dest('dist/fonts/')
-  ]);
+function copyAssets(cb) {
+  pump([gulp.src("src/assets/**/*"), gulp.dest("dist/assets/")], cb);
+}
 
-  // copy favicon
-  pump([
-    gulp.src('src/assets/img/favicon.ico'),
-    gulp.dest('dist/')
-  ]);
+function copyFonts(cb) {
+  pump([gulp.src("src/fonts/**/*"), gulp.dest("dist/fonts/")], cb);
+}
 
-  // copy svg icons into handlebars templates so that they can be imported
-  pump([
-    gulp.src('src/icons/**/*.svg'),
-    rename({
-      prefix: 'icon-',
-      extname: ".hbs"
-    }),
-    gulp.dest('src/templates/icons/')
-  ]);
-});
+function copyFavicon(cb) {
+  pump([gulp.src("src/assets/img/favicon.ico"), gulp.dest("dist/")], cb);
+}
 
-gulp.task('watch', function() {
-  runSequence(['build-html', 'build-js', 'build-css', 'build-assets']);
-  watch(['src/*.html', 'src/templates/**/*.hbs'], function() {
-    runSequence('build-html');
+function copyIcons(cb) {
+  pump(
+    [
+      gulp.src("src/icons/**/*.svg"),
+      rename({
+        prefix: "icon-",
+        extname: ".hbs",
+      }),
+      gulp.dest("src/templates/icons/"),
+    ],
+    cb
+  );
+}
+
+gulp.task("watch", gulp.parallel(watchHtml, watchJs, watchCss, watchAssets));
+
+function watchHtml() {
+  return gulp.watch(
+    ["src/*.html", "src/templates/**/*.hbs"],
+    { ignoreInitial: false },
+    function () {
+      const options = {
+        // partials (reusable files - are referenced like {{> header}})
+        batch: ["./src/templates/", "./src/templates/icons/"],
+      };
+
+      return gulp
+        .src("src/*.html")
+        .pipe(handlebars(templateData, options))
+        .pipe(gulp.dest("dist"))
+        .pipe(
+          sitemap({
+            siteUrl: process.env.URL || "http://quodera.com",
+          })
+        )
+        .pipe(gulp.dest("./dist"));
+    }
+  );
+}
+
+function watchJs() {
+  return gulp.watch("src/js/*.js", { ignoreInitial: false }, function (cb) {
+    const srcFiles = [
+      "src/js/libs/jquery-3.2.1.js",
+      "src/js/libs/*.js",
+      "src/js/scripts.js",
+    ];
+    pump(
+      [
+        gulp.src(srcFiles),
+        concat("bundle.js"),
+        gulpif(process.env.NODE_ENV === "production", uglify()),
+        gulp.dest("dist"),
+      ],
+      cb
+    );
   });
-  watch('src/js/*.js', function() {
-    runSequence('build-js');
-  });
-  watch(['src/css/libs/*.css', 'src/css/*.less'], function() {
-    runSequence('build-css');
-  });
-  watch(['src/assets/**/*', 'src/icons/**/*'], function() {
-    runSequence('build-assets');
-  });
-});
+}
 
-gulp.task('build', ['build-html', 'build-js', 'build-css', 'build-assets']);
+function watchCss() {
+  return gulp.watch(
+    ["src/css/libs/*.css", "src/css/*.less"],
+    { ignoreInitial: false },
+    gulp.series("compile-less", function (cb) {
+      const srcFiles = [
+        "src/css/libs/bootstrap.min.css",
+        "src/css/libs/*.css",
+        "src/css/styles.css",
+      ];
+      pump(
+        [
+          gulp.src(srcFiles),
+          concat("bundle.css"),
+          gulpif(process.env.NODE_ENV === "production", cleanCSS()),
+          gulp.dest("dist"),
+        ],
+        cb
+      );
+    })
+  );
+}
+
+function watchAssets() {
+  return gulp.watch(
+    ["src/assets/**/*", "src/icons/**/*"],
+    { ignoreInitial: false },
+    gulp.parallel(copyAssets, copyFonts, copyFavicon, copyIcons)
+  );
+}
+
+gulp.task(
+  "build",
+  gulp.parallel("build-html", "build-js", "build-css", "build-assets")
+);
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # Quodera Website
 
 ## Entwicklung
+
 ### Quelldateien & Überblick
+
 Die Entwicklungsdateien befinden sich im `src/` Ordner. Alle auf der Website angezeigten HTML Dateien befinden sich direkt in diesem Ordner. Sie verlinken auf globale Javascript und CSS Bundles, welche über `Gulp` gebaut wird.
 
 Wir nutzen wir [Handlebars](https://handlebarsjs.com/) Templates, um modulare Komponenten zu entwickeln. Diese werden in den HTML Dateien referenziert und über `Gulp` gebildet sowie in die HTML Dateien eingefügt. Die Templates liegen unter `templates/` und haben alle die `.hbs` Endung. Ein Template kann einfach über `{{> Dateiname ohne Endung}}` (z.B. `{{> header}}`) eingebunden werden (siehe auch die Handlebars Doku). Attribute kann man auch an die Templates weitergeben. Z.B. kann man an das head.hbs Template den Titel und die Beschreibung einer Seite weitergeben: `{{> head title="Das sind unsere Leistungen" description="Unsere Leistungen sind sehr toll."}}`. Wenn es sich um mehr Text handelt, wie bei den _Anwendungsgebieten_, dann steht der Text in `data/context.json`. Auf diesen kann man dann einfach in Handlebars über den Schlüsselnamen im JSON zugreifen (z.B. servicesApplicationAreas).
@@ -13,6 +15,7 @@ Für mehr Dynamik in CSS nutzen wir Less. Dadurch können wir pro Komponente ein
 Unser JavaScript Code steht unter `js/`. Wir haben aktuell sehr wenig Code, sodass es sich nicht lohnt ihn in Koponenten zu zerlegen. Alle Fremdbibliotheken wie z.B. jQuery befinden sich in `js/libs/`. Ein `Gulp` Task packt alle JavaScript Dateien zusammen in eine `bundle.js` Datei, die alle HTML Dateien referenzieren.
 
 Hier nochmal ein Dateiüberblick:
+
 - Bilder in `assets/img/`
 - Less und CSS Dateien in `css/` und Fremdbibliotheken in `css/libs/`
 - Daten für die Handlebars Templates in `data/`
@@ -36,47 +39,23 @@ Alles wird über `Gulp` gebaut und befindet sich in `gulpfile.js`. Nach dem erfo
 **In der Entwicklung sollte jedoch alles über npm ausgeführt werden!** Führt deshalb folgendes aus:
 
 Buildprozess starten und index.html im Browser öffnen
+
 ```
 npm start
 ```
+
 Buildergebnis löschen
+
 ```
 npm run clean
 ```
 
-Development Build ausführen
-```
-npm run build:dev
-```
-
 Production Build ausführen
+
 ```
-npm run build:prod
+npm run build
 ```
 
 ## Deployment
 
-Wir deployen unsere Website, indem wir die gebauten Dateien in zwei S3 Bucket hochladen: Eins für den Development und eins für den Production Build. **Dafür braucht man jedoch die AWS Rechte in Form von Credentials einer IAM Rolle. Diese bekommt ihr von Lucas bei Bedarf. Ihr müsst in eurem `/home/XXX` Ordner (z.B. /home/lucas/) den Ordner `.aws` anlegen und die credentials dort ablegen. Dann führt ihr `npm run install:aws_cli` aus (ihr braucht dafür pip).**
-
-Folgendes müsst ihr für das Deployment ausführen:
-
-Developement Deployment
-```
-npm run deploy:dev
-```
-
-Production Deployment
-```
-npm run deploy:prod
-```
-
-Es wird danach immer der Browser mit der Startseite geöffnet, wodurch ihr testen könnt, ob das Deployment erfolgreich war und keine Fehler vorhanden sind.
-
-### Website Adressen
-
-- Lokal: File manuell im Browser öffnen
-- Dev in S3: http://quodera.com-dev.s3-website.eu-central-1.amazonaws.com
-- Prod in S3: http://quodera.com.s3-website.eu-central-1.amazonaws.com
-- Prod mit Cloudfront https://www.quodera.com
-
-Achtung: *Falls Änderungen sofort redistributiert werden sollen, CloudFront Cache über AWS Webinterface löschen*
+Die Seite wird als Github Page deployed.
